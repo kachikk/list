fcba70923a583a959d9316907c877977f56956ee
kachikk <onyekachikk@yahoo.com>
2014-05-09 00:42:08 +0100

    model

---
+++ db/migrate/20140508233033_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.float :latitude
+      t.float :longitude
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
